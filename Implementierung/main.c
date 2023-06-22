a056537072b206eb3347c549dd740068b231f7c4
Ryan Kafoor <kafo@in.tum.de>
2023-06-22 11:46:38 +0200

    crypt implemented

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -80,17 +80,87 @@
    }
 
 
+
+    uint32_t rotate_bits_1(uint32_t number, uint8_t i){
+      return (number << i) | (number >> (32 -i));
+    }
+
+    //naive as possible
+    void transponse( uint32_t array[16]){
+
+      uint32_t temp[16];
+
+      temp[0] = array[0];
+      temp[4] = array[1];
+      temp[8] = array[2];
+      temp[12] = array[3];
+      temp[1] = array[4];
+      temp[5] = array[5];
+      temp[9] = array[6];
+      temp[13] = array[7];
+      temp[2] = array[8];
+      temp[6] = array[9];
+      temp[10] = array[10];
+      temp[14] = array[11];
+      temp[3] = array[12];
+      temp[7] = array[13];
+      temp[11] = array[14];
+      temp[15] = array[15];
+
+      for(int i = 0; i < 16; i++) {
+        array[i] = temp[i];
+      }
+
+    }
+
+
    //TODO: Naive C implementation without intrinsics
 
 
     void salsa20_core_1(uint32_t output[16], const uint32_t input[16]){
 
+      for (int i = 0; i < 16; i++) {
+        output[i] = input[i];
+    }
+
+      for (uint8_t i = 0; i < 20; i++)
+      {
+        output[4] = rotate_bits_1((output[0] + output[12]), 7) ^ output[4];
+        output[9] = rotate_bits_1((output[5] + output[1]), 7) ^ output[9];       
+        output[14] = rotate_bits_1((output[10] + output[6]), 7) ^ output[14];       
+        output[3] = rotate_bits_1((output[15] + output[11]), 7) ^ output[3];       
+
+        output[8] = rotate_bits_1((output[0] + output[4]), 9) ^ output[8];
+        output[13] = rotate_bits_1((output[5] + output[9]), 9) ^ output[13];       
+        output[2] = rotate_bits_1((output[10] + output[14]), 9) ^ output[2];       
+        output[7] = rotate_bits_1((output[15] + output[3]), 9) ^ output[7]; 
+      
+        output[12] = rotate_bits_1((output[8] + output[4]), 13) ^ output[12];
+        output[1] = rotate_bits_1((output[13] + output[9]), 13) ^ output[1];       
+        output[6] = rotate_bits_1((output[2] + output[14]), 13) ^ output[6];       
+        output[11] = rotate_bits_1((output[7] + output[3]), 13) ^ output[11];
+        
+        output[0] = rotate_bits_1((output[12] + output[8]), 18) ^ output[0];
+        output[5] = rotate_bits_1((output[1] + output[13]), 18) ^ output[5];       
+        output[10] = rotate_bits_1((output[6] + output[2]), 18) ^ output[10];       
+        output[15] = rotate_bits_1((output[11] + output[7]), 18) ^ output[15];
+      
+        transponse(output);
+      }
+
+      for (uint8_t i = 0; i < 16; i++)
+      {
+        output[i] += input[i]; 
+      }
+      
+      
 
     }
 
 
-    void salsa20_crypt(size_t mlen, const uint8_t msg[mlen], uint8_t cipher[mlen], uint32_t key[8], uint64_t iv){
-        size_t coreCounter = mlen / 64;
+    void salsa20_crypt_1(size_t mlen, const uint8_t msg[mlen], uint8_t cipher[mlen], uint32_t key[8], uint64_t iv){
+      
+      size_t coreCounter = mlen / 64;
       if (mlen % 64 != 0)
       {
         coreCounter++;
@@ -98,7 +168,8 @@
       uint32_t inputMatrix[16];
       uint32_t outputMatrix[16];
       uint32_t *outputPointer = outputMatrix;
-
+      
+      
       //Assigning const values
       inputMatrix[0]=0x61707865;
       inputMatrix[5]=0x3320646e;
