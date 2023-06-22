6b780d6eb6db3e0ce5406ee6eebadacf82c2dc35
Ryan Kafoor <ge48hax@mytum.de>
2023-06-22 14:06:52 +0200

    .

---
+++ Implementierung/intrinsic_impl.c
@@ -0,0 +1,20 @@
+
+
+
+//TODO: C implementation with intrinsics
+
+//Maybe much efficienter bit-rotating function?
+
+void transponse_2( uint32_t array[16]){
+
+}
+
+void salsa20_core_2(uint32_t output[16], const uint32_t input[16]){
+
+}
+
+void salsa20_crypt_2(size_t mlen, const uint8_t msg[mlen], uint8_t cipher[mlen], uint32_t key[8], uint64_t iv){
+
+}
+
+
